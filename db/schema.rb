0a417368cd48fca865298f004e76cf1250218623
Diego de Camargo Alvarenga <diegodecamargoalvarenga@ub-aqa-46.w3s.local>
2014-10-22 16:04:58 -0200

    Sale/Item/Devise

---
+++ db/schema.rb
@@ -0,0 +1,115 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141022174824) do
+
+  create_table "customers", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.string   "phone_number"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.boolean  "published",     default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "item_categories", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", force: true do |t|
+    t.string   "sku"
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price",            precision: 8, scale: 2
+    t.integer  "stock_amount"
+    t.integer  "amount_sold",                              default: 0
+    t.decimal  "cost_price",       precision: 8, scale: 2
+    t.boolean  "published",                                default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "item_category_id"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "item_id"
+    t.integer  "quantity",                            default: 1
+    t.decimal  "price",       precision: 8, scale: 2
+    t.decimal  "total_price", precision: 8, scale: 2
+    t.integer  "sale_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "payments", force: true do |t|
+    t.integer  "sale_id"
+    t.decimal  "amount",       precision: 8, scale: 2
+    t.string   "payment_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sales", force: true do |t|
+    t.decimal  "amount",           precision: 8,  scale: 2
+    t.decimal  "total_amount",     precision: 8,  scale: 2
+    t.decimal  "remaining_amount", precision: 10, scale: 0
+    t.decimal  "discount",         precision: 8,  scale: 2
+    t.decimal  "tax",              precision: 8,  scale: 2
+    t.integer  "customer_id"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "store_configurations", force: true do |t|
+    t.string   "store_name"
+    t.text     "store_description"
+    t.string   "email_address"
+    t.string   "phone_number"
+    t.string   "website_url"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.integer  "currency"
+    t.decimal  "tax_rate",          precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "usuarios", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
+  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
+
+end
