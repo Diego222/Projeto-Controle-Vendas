921f8bda42c22337f0cd06df2a31fbf8de538342
Diego de Camargo Alvarenga <diegodecamargoalvarenga@ub-aqa-46.w3s.local>
2014-11-14 12:39:33 -0200

    Editar usuário

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -4,6 +4,7 @@ class ItemCategory < ActiveRecord::Base
 	validates_uniqueness_of :name, :message => "Nome já cadastrado"
 	validates_length_of :name, :maximum => 50, :message => "Limite de caracteres ultrapassado (max: 50)"
 
+	default_scope :order => 'name ASC'
 	has_many :items
 
 end
