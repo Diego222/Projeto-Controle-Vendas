ce797c4e4c881a0846d1e8cd936146fdb3b0a74c
Diego de Camargo Alvarenga <diegodecamargoalvarenga@ub-aqa-46.w3s.local>
2014-11-26 10:17:04 -0200

    DataBase

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,8 @@ PushvendorPos::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'diego.alvarenga58@gmail.com',
-    password:             '718293di',
+    user_name:            'artnissitapecaria@gmail.com',
+    password:             'artnissi123',
     authentication:       'plain',
    # enable_starttls_auto: true  
     }
